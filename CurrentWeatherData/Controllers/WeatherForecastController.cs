b612e27c361e27239207132d663ebe1f7bd12273
srivastavaanuj <38906280+srivastavaanuj@users.noreply.github.com>
2022-01-23 22:55:01 +1100

    Add files via upload

---
+++ CurrentWeatherData/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using CurrentWeatherData.Services;
+using Microsoft.AspNetCore.Http;
+
+namespace CurrentWeatherData.Controllers
+{
+    [ApiController]
+    [Route("api/weatherforecast")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ICurrentWeatherService _currentWeatherService;
+
+        public WeatherForecastController(ICurrentWeatherService currentWeatherService )
+        {
+            _currentWeatherService = currentWeatherService;
+        }
+
+        [HttpGet("City/{city}/Country/{country}")]
+        public async Task<IActionResult> GetCurrentWeather(string city,string country)  
+        {
+                try
+                {
+                    var query = $"{city},{country}";
+                    var weatherResponse = await _currentWeatherService.GetWeatherReport(query);
+                    if (weatherResponse.Weather == null) return NotFound("No Result found!");
+                    return Ok(weatherResponse);
+                }
+                catch (HttpRequestException httpRequestException)
+                {
+                     return this.StatusCode(StatusCodes.Status500InternalServerError, httpRequestException.Message);
+                }
+        }
+    }
+}
---
+++ CurrentWeatherData/Middleware/ApiKeyMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrentWeatherData.Middleware 
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+        private const string API_KEY = "ApiKey";
+        public ApiKeyMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(API_KEY, out var clientApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Api Key was not provided.");
+                return;
+            }
+
+            var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+
+            var apiKey = appSettings.GetValue<string>(API_KEY);
+
+            if (!apiKey.Equals(clientApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync
+                      ("Unauthorized client.");
+                return;
+            }
+
+            await _requestDelegate(context);
+        }
+    }
+}
